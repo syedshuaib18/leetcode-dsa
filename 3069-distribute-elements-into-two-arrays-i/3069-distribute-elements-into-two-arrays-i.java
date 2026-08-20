ae725addd3c52977a353cee4b61acf1e17bd1885
syedshuaib18 <syedshuaibt7@gmail.com>
2026-08-20 13:27:03 +0530

    Time: 1 ms (98.08%), Space: 46.3 MB (95.43%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,20 +1,28 @@
 class Solution {
     public int[] resultArray(int[] nums) {
-        int[] result =new int [nums.length];
-        int[]arr1=new int [nums.length];
-        int []arr2=new int [nums.length];
-        int a = 0, b = 0;
+        int n =nums.length;
+        int []arr1=new int [n];
+        int []arr2=new int [n];
 
-        arr1[a++] = nums[0];
-        arr2[b++] = nums[1];
-                for (int i = 2; i < nums.length; i++) {
-            if (arr1[a - 1] > arr2[b - 1])
-                arr1[a++] = nums[i];
-            else
-                arr2[b++] = nums[i];
-        }for (int i = 0; i < a; i++) result[i] = arr1[i];
-        for (int i = 0; i < b; i++) result[a + i] = arr2[i];
+        int a=0;int b=0;
+        arr1[a++]=nums[0];
+        arr2[b++]=nums[1];
+        for(int i=2;i<n;i++){
+            if(arr1[a-1]>arr2[b-1]){
+                arr1[a++]=nums[i];
+            }
+            else{
+                arr2[b++]=nums[i];
+            }
+        }
+        int []result=new int[n];
+        for(int i=0;i<a;i++){
+            result[i]=arr1[i];
 
+        }
+        for(int i=0;i<b;i++){
+            result[i+a]=arr2[i];
+        }
         return result;
     }
 }
