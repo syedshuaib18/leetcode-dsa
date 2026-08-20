263046645834c75034a594bc58cb4724ff4a715d
syedshuaib18 <syedshuaibt7@gmail.com>
2026-08-20 17:06:10 +0530

    Time: 1 ms (98.08%), Space: 46.9 MB (12.74%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -3,10 +3,9 @@ class Solution {
         int n =nums.length;
         int []arr1=new int [n];
         int []arr2=new int [n];
-
-        int a=0;int b=0;
+        int a=0;int b =0;
         arr1[a++]=nums[0];
-        arr2[b++]=nums[1];
+        arr2[b++]= nums[1];
         for(int i=2;i<n;i++){
             if(arr1[a-1]>arr2[b-1]){
                 arr1[a++]=nums[i];
@@ -15,14 +14,14 @@ class Solution {
                 arr2[b++]=nums[i];
             }
         }
-        int []result=new int[n];
+        int []result=new int [n];
         for(int i=0;i<a;i++){
             result[i]=arr1[i];
-
         }
         for(int i=0;i<b;i++){
             result[i+a]=arr2[i];
-        }
-        return result;
+                    }
+                    return result;
+        
     }
 }
