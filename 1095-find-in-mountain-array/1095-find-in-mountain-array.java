659334a9d6a58ddcb02b57e84b1dee9cb4f5c615
syedshuaib18 <syedshuaibt7@gmail.com>
2026-09-01 19:26:14 +0530

    Time: 0 ms (100%), Space: 46.8 MB (8.24%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.java
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -1,63 +1,70 @@
-/**
- * // This is MountainArray's API interface.
- * // You should not implement it, or speculate about its implementation
- * interface MountainArray {
- *     public int get(int index) {}
- *     public int length() {}
- * }
- */
+
  
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
-        int left=0;
+        
+        int  left=0;
         int right=mountainArr.length()-1;
-        int peak=0;
-        //finding peak
+        int peak =findPeak(mountainArr,left,right);
+        int ans=searchAscending(mountainArr,target,left,peak);
+        if(ans!=-1)return ans;
+        return searchDescending(mountainArr,target,peak+1,right);
+    }
+    private int findPeak(MountainArray a,int left,int right){
         while(left<right){
             int mid=left+(right-left)/2;
-            if(mountainArr.get(mid)<mountainArr.get(mid+1)){
+            if(a.get(mid)<a.get(mid+1)){
                 left=mid+1;
             }
             else{
                 right=mid;
             }
-             peak=right;
         }
-
-        //run binary search from start to peak
-        
-        int start =0;
-        int end=peak;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(mountainArr.get(mid)==target){
-                return mid;
-            }
-            else if(mountainArr.get(mid)>target){
-            end=mid-1;
-            }
-            else {
-                start=mid+1;
-            }
-
-        }
-        int st=peak;
-        int en=mountainArr.length()-1;
-        while(st<=en){
-            int mid=st+(en-st)/2;
-            if(mountainArr.get(mid)==target){
-                return mid;
-            }
-            else if(mountainArr.get(mid)>target){
-            st=mid+1;
-            }
-            else {
-                en=mid-1;
-                
-            }
-
-        }
-
-        return -1;
+        return left;
     }
-}
+    private int searchAscending(MountainArray a,int target,int left,int right ){
+     
+     while(left<=right){
+        int mid=left+(right-left)/2;
+         if(a.get(mid)==target){
+             return mid;}
+             else if(a.get(mid)<target){
+                 
+             left=mid+1;
+         }
+         else{
+             right=mid-1;
+         }
+     }
+     
+     
+     return -1;
+        
+    }
+    private int searchDescending(MountainArray a,int target,int left,int right){
+        
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(a.get(mid)==target){
+                return mid;
+            }
+            else if(a.get(mid)>target){
+                 
+             left=mid+1;
+         }
+         else{
+             right=mid-1;
+         }
+         }
+         return -1;
+     
+          }
+        
+        
+        
+
+        
+        
+    }
+    
+    
